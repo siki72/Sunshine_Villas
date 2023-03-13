ec52de649ed478c98f7c080d98026092dd164263
siki72 <missoum.ali.hichem@gmail.com>
2023-03-13 18:29:50 +0100

    link-show-bmybookings

--- client/src/pages/Mybookings.jsx
+++ client/src/pages/Mybookings.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { UserContext } from "../users/UserContext.jsx";
 import index from "../components/Routes/index";
 import { format } from "date-fns";
+import { Link } from "react-router-dom";
 
 const Mybookings = () => {
   const { user } = useContext(UserContext);
@@ -14,7 +15,6 @@ const Mybookings = () => {
     })
       .then((resp) => resp.json())
       .then((data) => setMyBookings(data));
-    console.log(myBookings);
   }, []);
 
   return (
@@ -23,44 +23,48 @@ const Mybookings = () => {
         <div className="loader">chargement ,,</div>
       ) : (
         myBookings.map((resa, index) => (
-          <div key={index} className="reservation-grid">
-            <div
-              style={{
-                backgroundImage: `url(${resa.url})`,
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "cover",
-                objectFit: "cover",
-              }}
-              className="res-img"
-            ></div>
-            <div className="res-infos">
-              <h2>Your booking details : </h2>
-              <h3>
-                Villa: <span> {resa.name}</span>{" "}
-              </h3>
+          <Link to={`/villas/${resa.id}`} key={index}>
+            <div className="reservation-grid">
+              <div
+                style={{
+                  backgroundImage: `url(${resa.url})`,
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                  objectFit: "cover",
+                }}
+                className="res-img"
+              ></div>
+              <div className="res-infos">
+                <h2>Your booking details : </h2>
+                <h3>
+                  Villa: <span> {resa.name}</span>{" "}
+                </h3>
 
-              <div className="res-date">
-                <div className="start">
-                  <p>Check-in</p>
-                  <h4>
-                    {" "}
-                    {format(new Date(resa.start_date), "EEEE dd/MM/yyyy")}
-                  </h4>
-                  <p>From 4:00 PM</p>
+                <div className="res-date">
+                  <div className="start">
+                    <p>Check-in</p>
+                    <h4>
+                      {" "}
+                      {format(new Date(resa.start_date), "EEEE dd/MM/yyyy")}
+                    </h4>
+                    <p>From 4:00 PM</p>
+                  </div>
+                  <div className="end">
+                    <p>Check-out</p>
+                    <h4>
+                      {format(new Date(resa.end_date), "EEEE dd/MM/yyyy")}
+                    </h4>
+                    <p>Until 11:00 AM</p>
+                  </div>
                 </div>
-                <div className="end">
-                  <p>Check-out</p>
-                  <h4>{format(new Date(resa.end_date), "EEEE dd/MM/yyyy")}</h4>
-                  <p>Until 11:00 AM</p>
+                <div className="totals">
+                  <p>Total length of stay : </p>
+                  <h4>{resa.nights} nights</h4>
+                  <div>Total price : {resa.total_price} euros </div>
                 </div>
-              </div>
-              <div className="totals">
-                <p>Total length of stay : </p>
-                <h4>{resa.nights} nights</h4>
-                <div>Total price : {resa.total_price} euros </div>
               </div>
             </div>
-          </div>
+          </Link>
         ))
       )}
     </div>
