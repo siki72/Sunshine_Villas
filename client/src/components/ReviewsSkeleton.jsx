864e911e29d502af1f72cc78f34b486b6b840e5f
Ali <121733733+siki72@users.noreply.github.com>
2023-04-28 17:03:47 +0200

    skeleton

--- client/src/components/ReviewsSkeleton.jsx
+++ client/src/components/ReviewsSkeleton.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const ReviewsSkeleton = () => {
   return (
-    <div className="flex animate-pulse w-80">
+    <div className="flex animate-pulse w-64">
       <div className="flex-shrink-0 ml-4">
-        <span className="w-12 h-12 ml-4 block bg-gray-200 rounded-full dark:bg-gray-700 "></span>
+        <span className="w-12 h-12 ml-4 block bg-gray-200 rounded-full dark:bg-gray-700 mr-10"></span>
       </div>
       <div className="w-full">
-        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[480px] mb-2.5"></div>
+        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-60 mb-4"></div>
+        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[700px] mb-2.5"></div>
         <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[440px] mb-2.5"></div>
-        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[460px] mb-2.5"></div>
-        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[700px] mb-2.5"></div>
+        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[700px] mb-2.5"></div>
+        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[700px]"></div>
       </div>
       <span className="sr-only">Loading...</span>
     </div>
